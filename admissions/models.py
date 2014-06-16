898f65eb17b58c37ac21425c2cc9b83af0ade142
Akshay Dewan <akudewan@gmail.com>
2014-06-16 23:57:13 +0530

    Created models for Student and AdmissionRequest

---
+++ admissions/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Student(models.Model):
+        GENDERS = (
+                ('M', 'Male'),
+                ('F', 'Female')
+        )
+	name = models.CharField(max_length=200)
+        gender = models.CharField(max_length=1, choices=GENDERS)
+        age = models.IntegerField()
+        university = models.CharField(max_length=200)
+        department = models.CharField(max_length=200)
+        percent_sem1 = models.DecimalField(max_digits=4, decimal_places=2)
+        percent_sem2 = models.DecimalField(max_digits=4, decimal_places=2)
+        percent_sem3 = models.DecimalField(max_digits=4, decimal_places=2)
+        percent_sem4 = models.DecimalField(max_digits=4, decimal_places=2)
+        
+class AdmissionRequest(models.Model):
+        STATUS = (
+                ('PENDING', 'Pending'),
+                ('APPROVED', 'Approved'),
+                ('REJECTED', 'Rejected')
+        )
+        student = models.ForeignKey(Student)
+        date = models.DateField()
+        department = models.CharField(max_length=200)
+        processed_by = models.CharField(max_length=200)
