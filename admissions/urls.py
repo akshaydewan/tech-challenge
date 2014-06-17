bd44142ecbb485fdcb07556fe49dc57b136812ed
Akshay Dewan <akudewan@gmail.com>
2014-06-17 14:50:53 +0530

    Added views and styles from twitter-bootstrap

--- admissions/urls.py
+++ admissions/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from admissions import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index),
+    url(r'^generate/', views.generateData),
 )
--- admissions/views.py
+++ admissions/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
+    return render(request, 'admissions/index.html')
+
+def generateData(request):
+    return render(request, 'admissions/generate.html')
+
